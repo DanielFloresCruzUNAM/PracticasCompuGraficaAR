c20d6d970113c2877d12b2a65b2b72a4668f64e0
Daniel Flores Cruz <daniel997cruz@gmail.com>
2025-04-10 19:58:49 -0600

    Añadiendo ejecutable - Practica5

---
+++ P5/Ejecutable/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,60 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void ChangeTextures_ChangeColor_BTN_m75EA40221FFBDDA2830F06A259B0606C95DEC944 (void);
+extern void ChangeTextures_ChangeTexture_BTN_mE825D4F0F754B8C2B759C91E097DD3564B63F001 (void);
+extern void ChangeTextures_ApplyTextures_m8650A2F37D0223AF83B12079948066D86F5725C3 (void);
+extern void ChangeTextures__ctor_m811CD1C97E6D841A85DD98238484CA365616E9DC (void);
+extern void VuforiaLicense_GetLicenseKey_m796D51E5EF1C53F3D62BA4EA8806AC1FB1A5E3C4 (void);
+extern void VuforiaLicense__ctor_m094B92B3323DFFD79ADB696474D5D158C95E291B (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[8] = 
+{
+	ChangeTextures_ChangeColor_BTN_m75EA40221FFBDDA2830F06A259B0606C95DEC944,
+	ChangeTextures_ChangeTexture_BTN_mE825D4F0F754B8C2B759C91E097DD3564B63F001,
+	ChangeTextures_ApplyTextures_m8650A2F37D0223AF83B12079948066D86F5725C3,
+	ChangeTextures__ctor_m811CD1C97E6D841A85DD98238484CA365616E9DC,
+	VuforiaLicense_GetLicenseKey_m796D51E5EF1C53F3D62BA4EA8806AC1FB1A5E3C4,
+	VuforiaLicense__ctor_m094B92B3323DFFD79ADB696474D5D158C95E291B,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[8] = 
+{
+	14802,
+	14802,
+	14802,
+	14802,
+	23235,
+	14802,
+	23344,
+	14802,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	8,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
